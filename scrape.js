8cc98f1f0a1851bd514ee01473c15f575b7e974a
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-18 13:21:49 -0700

    Scrape trivia question data for country question clues

--- scrape.js
+++ scrape.js
@@ -3,29 +3,24 @@ const fs = require('fs')
 const nightmare = Nightmare({ show: true });
 
 nightmare
-  .goto('http://jservice.io/popular/1361')
+  .goto('http://jservice.io/popular/8453')
   .evaluate( function () {
     const questionNodes = document.querySelectorAll('tr')
     const list = [].slice.call(questionNodes)
-    const totalQuestions = []
+    const totalQuestions = {}
 
     list.forEach(row => {
       const dataNodes = row.childNodes
-      // const dataNodes = document.querySelectorAll('td')
       const textList = [].slice.call(dataNodes)
-      const question = {
-        question: textList[1].textContent,
-        country: textList[3].textContent
-      }
 
-      totalQuestions.push(question)
+      totalQuestions[textList[3].textContent] = textList[1].textContent
     })
 
     return totalQuestions
   })
   .end()
   .then(function(questions){
-    fs.writeFileSync('testOutput.json', JSON.stringify(questions));
+    fs.writeFileSync('newspaper2.json', JSON.stringify(questions));
     console.log('Done!');
   })
   .catch(function(err){
