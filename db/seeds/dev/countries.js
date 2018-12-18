13136077a11b8d3126470911d7a4f1fcb5a275f9
bhayek12@gmail.com <bhayek12@gmail.com>
2018-12-18 15:50:30 -0700

    Create seed file

---
+++ db/seeds/dev/countries.js
@@ -0,0 +1,38 @@
+const countries = require('../../../countryData')
+
+const createCountries = (knex, country) => {
+  return knex('countries').insert({
+    name: country.name, 
+    flag: country.flag, 
+    outline: country.outline, 
+  }, 'id')
+  .then(countryId => {
+    let factPromises = []
+
+    country.questions.forEach(fact => {
+      factPromises.push(createFacts(knex, {
+        country_fact: fact,
+        country_id: countryId[0]
+      }))
+    });
+    return Promise.all(factPromises)
+  })
+}
+
+const createFacts = (knex, fact) => {
+  return knex('facts').insert(fact);
+}
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('facts').del()
+    .then(() => knex('countries').del())
+    .then(() => {
+      let countryPromises = countries.map(country => {
+        return createCountries(knex, country)
+      })
+        return Promise.all(countryPromises)
+        })
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+  };
