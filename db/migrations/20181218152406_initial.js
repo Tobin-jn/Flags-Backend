eab31fd4517a810ce2a277bd9caa593fbf4513ae
bhayek12@gmail.com <bhayek12@gmail.com>
2018-12-18 15:29:40 -0700

    Add first migration

---
+++ db/migrations/20181218152406_initial.js
@@ -0,0 +1,30 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+      knex.schema.createTable('countries', function(table) {
+        table.increments('id').primary();
+        table.string('name');
+        table.string('flag');
+        table.string('country_outline');
+  
+        table.timestamps(true, true);
+      }),
+  
+      knex.schema.createTable('facts', function(table) {
+        table.increments('id').primary();
+        table.string('country_fact');
+        table.integer('country_id').unsigned()
+        table.foreign('country_id')
+          .references('countries.id');
+  
+        table.timestamps(true, true);
+      })
+    ])
+  };
+  
+  exports.down = function(knex, Promise) {
+    return Promise.all([
+      knex.schema.dropTable('facts'),
+      knex.schema.dropTable('countries')
+    ]);
+  };
