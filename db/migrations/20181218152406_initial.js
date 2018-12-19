11ae8a21ffd1e7d1d3fa3d6d4f73c8ffc80f0099
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-18 19:50:24 -0700

    Update Migration with user table

--- db/migrations/20181218152406_initial.js
+++ db/migrations/20181218152406_initial.js
@@ -18,12 +18,24 @@ exports.up = function(knex, Promise) {
           .references('countries.id');
   
         table.timestamps(true, true);
+      }),
+
+      knex.schema.createTable('users', function(table) {
+        table.increments('id').primary();
+        table.string('username').unique().notNullable();
+        table.string('password').notNullable();
+        table.boolean('admin').notNullable().defaultTo(false);
+        table.integer('points')
+
+        table.timestamps(true, true);
+
       })
     ])
   };
   
   exports.down = function(knex, Promise) {
     return Promise.all([
+      knex.schema.dropTable('users'),
       knex.schema.dropTable('facts'),
       knex.schema.dropTable('countries')
     ]);
