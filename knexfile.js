eb3350944adb97e013b5e27ef3f425ce5153e06f
Tobin Nelson <tobin.j.n@gmail.com>
2018-12-18 22:07:58 -0700

    Update config

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
@@ -28,7 +26,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     },
